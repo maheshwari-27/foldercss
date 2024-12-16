9e0f8fb41fe0ecf896afda25fdcfd5864e5de84c
kaderu-27 <kaderuuma@gmail.com>
2024-12-16 20:21:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+/*let a= +prompt("enter the first value");
+let b=+prompt("enter the second value");
+let c =a/b;
+confirm("before dividing  the values is it ok to confirm or not");
+alert ("the division "+c);
+console.log("division");*/
+/*let t= +prompt("enter the first value");
+let y=+prompt("enter the second value");
+let e=t+y;
+confirm("before adding doyou want confirm or not");
+alert("addtion is "+e);
+console.log("addition");*/
+let a=2.555;
+console.log(a);
+console.log(typeof (a));
