06e449bc0234ce82c1b6e44a4611af58b9dd69aa
kaderu-27 <kaderuuma@gmail.com>
2024-12-20 16:57:57 +0530

    first commit

---
+++ basics/undefined/null/operators/script.js
@@ -0,0 +1,16 @@
+let a=50,b=10;
+console.log(`the addtion of ${a} and ${b} is ${a+b}`);
+console.log(`the subtraction of ${a} and ${b} is ${a-b}`);
+console.log(`the multiplication of ${a} and ${b} is ${a*b}`);
+console.log(`the division of ${a} and ${b} and ${a/b}`);
+console.log(`the modules of ${a} and ${b} and ${a%b}`);
+/*relational operators*/
+let a1=50,b1='10';
+console.log(a>b);//true
+console.log(a<b);//false
+console.log(a<=b);//false
+console.log(a>=b);//true
+console.log(a==b);//false
+console.log(a!=b);//true
+console.log(a===b);//false
+console.log(a!==b);//true
---
+++ basics/undefined/null/script.js
@@ -0,0 +1,5 @@
+console.log(a);
+let a=50;
+let b= 20;
+console.log(a);
+console.log(b);
